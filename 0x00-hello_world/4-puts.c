52d2a0944ad6ecceb13fe640a26970f041dbc880
braveking12 <davidkadofficial@gmail.com>
2023-06-15 14:06:28 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main--this programe prints a line with the puts function
+ * Return:0 'completed'*/
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
