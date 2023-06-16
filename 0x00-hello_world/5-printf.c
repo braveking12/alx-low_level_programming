76125ba2721cb1c618bbcd5316bb8684d7a70f5a
braveking12 <davidkadofficial@gmail.com>
2023-06-15 17:29:49 -0700

    5. Hello, printf tasks done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that print the line using the printf function command in c
+ * Return: 0 (completed)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
