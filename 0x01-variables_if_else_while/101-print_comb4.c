0b22d4a7d27ca88fe3adf15d555be027d740d068
braveking12 <davidkadofficial@gmail.com>
2023-06-16 17:03:48 -0700

    a program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints all possible combinations,
+ *  of two two-digit numbers.
+ *  thw two numbers are represented by a space, seperated by comma,
+ *  printed in ascending order.
+ *  putchar can only be used a maximum of eight 8 times
+ *  char variable is not allowed
+ *  Return: value 0 (Succesful).
+ */
+int main(void)
+{
+	int digt_comb1, digt_comb2, digt_comb3;
+
+	for (digt_comb1 = 0; digt_comb1 < 8; digt_comb1++)
+	{
+		for (digt_comb2 = digt_comb1 + 1; digt_comb2 < 9; digt_comb2++)
+		{
+			for (digt_comb3 = digt_comb2 + 1; digt_comb3 < 10; digt_comb3++)
+			{
+				putchar((digt_comb1 % 10) + '0');
+				putchar((digt_comb2 % 10) + '0');
+				putchar((digt_comb3 % 10) + '0');
+
+				if (digt_comb1 == 7 && digt_comb2 == 8 && digt_comb3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
