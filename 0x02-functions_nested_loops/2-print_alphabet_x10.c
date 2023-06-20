ebba4672dedec54870cca09858bd2107fa309604
braveking12 <davidkadofficial@gmail.com>
2023-06-20 13:12:08 -0700

    10 x alphabet tasks done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
+ *followed by a new line,
+ * Return: value 0 (Successful).
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alphabet_x10;
+
+	while (i++ <= 9)
+	{
+		for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
+			_putchar(alphabet_x10);
+		_putchar('\n');
+	}
+}
