dce51399344552c4e45a287671466f0289665847
braveking12 <davidkadofficial@gmail.com>
2023-07-03 11:01:17 -0700

    2. strchr tasks done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  * using while loop
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
+{
+	int i = 0, k;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	for (k = 0; k <= i; k++)
+	{
+		if (c == s[k])
+		{
+			s += k;
+			return (s);
+		}
+	}
+	return ('\0');
+}
