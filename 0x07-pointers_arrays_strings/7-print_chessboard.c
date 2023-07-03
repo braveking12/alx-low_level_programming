b36e23edb7c86b8b992bd5b9e0f11cc6412a5e8a
braveking12 <davidkadofficial@gmail.com>
2023-07-03 11:22:05 -0700

    6. Chess is mental torture tasks done

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_chessboard - Print the chessboard
+  * @a: array of pieces
+  * using for loop
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[i][k]);
+		}
+
+		_putchar('\n');
+	}
+}
+
