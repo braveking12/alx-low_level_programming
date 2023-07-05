a5bf13823cf94cbca2fc69a0b74948f0deaab6c5
braveking12 <davidkadofficial@gmail.com>
2023-07-05 14:23:04 -0700

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int find_sqrt(int input_num, int let);
+int _sqrt_recursion(int n);
+
+/**
+ * find_sqrt - Find the square root of a number.
+ * @input_num: The number to find the square root of.
+ * @let: The square root value to be tested.
+ * using if statement
+ *
+ * Return: Square root if found, otherwise -1.
+ */
+int find_sqrt(int input_num, int let)
+{
+	if ((let * let) == input_num)
+		return (let);
+
+	if (let == input_num / 2)
+		return (-1);
+
+	return (find_sqrt(input_num, let + 1));
+}
+
+/**
+ * _sqrt_recursion - Calculate the square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: Square root if found, otherwise -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int let = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, let));
+}
+
