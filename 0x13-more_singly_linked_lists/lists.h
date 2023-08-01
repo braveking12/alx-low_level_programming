dd0e0015c359c16e7a79e40ac33f9b296d910778
braveking12 <davidkadofficial@gmail.com>
2023-08-01 11:25:27 -0700

    header files

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,8 +11,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -36,5 +36,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-
-#endiif
+#endif
