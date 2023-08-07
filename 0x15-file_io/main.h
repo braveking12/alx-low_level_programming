694302576d0e8473ee28c67c590a752108ed4103
braveking12 <davidkadofficial@gmail.com>
2023-08-07 07:47:26 -0700

    header files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
