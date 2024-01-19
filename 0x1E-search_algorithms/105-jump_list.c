b8d0614cb9c5acbcd3d76d3a9f5b789153d20a6b
braveking12 <davidkadofficial@gmail.com>
2024-01-19 15:00:44 +0000

    all advance tasks

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for a value in a linked list
+ * of integers using the Jump search algorithm.
+ *
+ * @list: The input linked list.
+ * @size: The size of the linked list.
+ * @value: The value to search for.
+ *
+ * Return: The index of the value in the linked list.
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
